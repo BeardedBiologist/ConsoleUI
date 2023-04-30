f24010831fe9f7da4bc419cecc16dce02e49bff1
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-04-30 12:08:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  BREIF  
+//  Create a Console application with static class that
+//  handles calculations
+
+
+namespace Calulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
